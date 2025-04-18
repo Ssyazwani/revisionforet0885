c5949c20e21bf10d26e73220a5f65b0d55e750fa
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2025-04-19 00:32:46 +0800

    lesson1 practical

---
+++ prac1.java
@@ -0,0 +1,22 @@
+//Lesson 1
+//1.1 Continue to add codes to display “W” in large block letter which is made up of lower case "w", 
+//1.2 Design a Java application to display "Welcome! I'm Duke." In a message box.
+
+
+
+
+public class prac1 {
+
+    public static void main(String[] args){
+    //1.1
+        System.out.println("w   w   w");
+        System.out.println(" w w w w");
+        System.out.println("  w   w");
+
+
+    //1.2
+       System.out.println("\n Welcome! I'm Duke.");
+    }
+    
+
+}

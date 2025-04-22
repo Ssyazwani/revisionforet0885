3fff0c686875098a3d6002960f87df1d2ba2c0b7
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2025-04-22 19:06:23 +0800

    prac 3 example

---
+++ prac3.java
@@ -0,0 +1,31 @@
+// prac3 example
+
+import java.util.Scanner;
+
+public class prac3 {
+
+    public static void main(String[] args) {
+
+        String strOUT;
+
+        Scanner sc = new Scanner(System.in);
+        int luckynum = 7;
+
+        int x = sc.nextInt();
+
+        if (x==luckynum){
+            strOUT ="Congratulations!";
+            System.out.println(strOUT);
+
+        } else {
+
+            strOUT ="Too bad!";
+            System.out.println(strOUT);
+
+        }
+
+
+        
+    }
+    
+}

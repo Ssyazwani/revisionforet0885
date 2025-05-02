641c4dadd16fcfdaa144a8c219888cd3fb1c542f
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2025-05-02 17:37:03 +0800

    adding a small exercise

---
+++ findlongestword.java
@@ -0,0 +1,42 @@
+//Recalling that I did this exercise before, a small revision
+
+import java.util.Scanner;
+
+public class findlongestword {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter your sentence");
+
+        // String findLine = "Please stream Unnatural on Netflix";
+
+        // System.out.println("The line is "+ findLine);
+
+        String inputLine = sc.nextLine();
+
+        
+        String words[] = inputLine.split(" ");
+
+        String longestWord = "";
+
+        for (String word : words) {
+            if (word.length() > longestWord.length()) {
+                longestWord = word;
+            }
+        }
+
+        System.out.println("The longest word is: " + longestWord);
+
+        // String line = sc.nextLine();
+
+
+
+
+
+        
+        
+    }
+    
+}

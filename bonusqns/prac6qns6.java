7c5317fef8da9c5dfc3fd91f694769a746dee4a9
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2025-05-24 13:59:00 +0800

    added bonus prac6qnd6

---
+++ bonusqns/prac6qns6.java
@@ -0,0 +1,31 @@
+package bonusqns;
+//Q6.6   Use built in Java class Arrays static method sort(double [ ] a )  
+//to sort an 1D array {98, 47, 27, 68, 100, 9.5} ,then print out the sorted array’s element value one by one.
+
+import java.util.Arrays;
+
+public class prac6qns6 {
+
+    public static void main(String[] args) {
+         double [] array = {98, 47, 27, 68, 100, 9.5};
+
+          System.out.print("Unsorted array : ");
+
+          for(int i =0; i < array.length;i ++){
+            System.out.print(array[i]+ ",");
+         }
+
+        System.out.println();
+
+         System.out.print("Sorted array : ");
+         for(int i =0; i < array.length;i ++){
+            Arrays.sort(array);
+            System.out.print(array[i]+ ",");
+         }
+
+
+    }
+
+   
+    
+}

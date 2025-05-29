0d7ecaa21174861468312a25912acb500a5fd249
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2025-05-29 23:19:20 +0800

    did prac4q2

--- Chap4/prac4q2.java
+++ Chap4/prac4q2.java
@@ -1,17 +1,36 @@
 package Chap4;
 
+import java.util.Scanner;
+
 //4.2    Write a method with the following header that displays a message n times using for loop , then while, then do while loop. 
 // Write a test program to use this method.
 
-//Write a method with the following header that displays a message n times using for loop , then while, then do while loop.
-// Write a test program to use this method
+
 
 public class prac4q2 {
     
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter how many times you would like to display your message");
+
+        int n = sc.nextInt();
+
+        displayMessage(n);
+        
         
     }
+
+public static void displayMessage(int n) {
+    String message = "Welcome, Welcome, Dun Dun Dun";
+
+    for (int i = 0; i < n; i++) {
+        System.out.println(message);
+    }
+}
+
 
 }
 
